4bf03d92dfd91f61f14fee463f7c04e7bfdb8906
Dave-Lyrics <davidakintoye2008@gmail.com>
2026-04-01 17:27:43 +0100

    Changed some hardcoded data

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); //to parse req.body
+app.use(express.json({ limit: "5mb" })); //to parse req.body
 app.use(express.urlencoded({ extended: true })); //to parse urlencoded data(urlencoded)
 
 app.use(cookieParser()) // to parse cookies from incoming requests, making them available under req.cookies
